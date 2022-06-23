1817b040ee20566ffa67909650255ae0674e5655
Daniel Christofolli <danielchristofolli@gmail.com>
2022-06-23 14:07:38 -0300

    initial configs

---
+++ src/main/java/com/dchristofolli/webfluxessentials/WebfluxEssentialsApplication.java
@@ -0,0 +1,11 @@
+package com.dchristofolli.webfluxessentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxEssentialsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxEssentialsApplication.class, args);
+    }
+}
